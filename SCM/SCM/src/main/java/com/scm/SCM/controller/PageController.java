0e503e9fccadbc7b217078159d475d4858f50a22
Vaibhav Tambe <tambevaibhav207@gmail.com>
2024-09-11 16:54:16 +0530

    configure the application.propertyies file

---
+++ SCM/SCM/src/main/java/com/scm/SCM/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.scm.SCM.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+
+    public String home(Model model){
+        System.out.println("Home page handler");
+        model.addAttribute("name", "Vaibhav Tambe");
+        model.addAttribute("Youtube", "hb");
+        model.addAttribute("a", "b");
+        return "home";
+
+    }
+
+}
