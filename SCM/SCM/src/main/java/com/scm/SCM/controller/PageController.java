bbe213e17c9fa2a2d522beeb1a9b312e16bfac78
Vaibhav Tambe <tambevaibhav207@gmail.com>
2024-09-15 16:53:53 +0530

    appy the validation on user signup page

--- SCM/SCM/src/main/java/com/scm/SCM/controller/PageController.java
+++ SCM/SCM/src/main/java/com/scm/SCM/controller/PageController.java
@@ -3,6 +3,7 @@ package com.scm.SCM.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.scm.SCM.helpers.MessageType;
 import com.scm.SCM.services.UserServices;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -72,11 +74,19 @@ public class PageController {
 
     //processing register
     @RequestMapping(value = "/do-register",method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm ,HttpSession session){
+    public String processRegister(@Valid @ModelAttribute UserForm userForm ,
+      BindingResult rBindingResult,HttpSession session){
 
         
         System.out.println("Processinggggggggggg");
         System.out.println(userForm);
+
+
+        //validate form data
+
+        if(rBindingResult.hasErrors()){
+            return "register";
+        }
 
         //save to db
     
--- SCM/SCM/src/main/java/com/scm/SCM/forms/UserForm.java
+++ SCM/SCM/src/main/java/com/scm/SCM/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.SCM.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +18,18 @@ import lombok.ToString;
 @ToString
 
 public class UserForm {
+    @NotBlank(message = "username is required")
+    @Size(min = 3,message = "min 3 charter required")
     private String name;
+
+    @Email(message = "Invalid Email Address")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min=6,message = "Min 6 charter is required")
     private String password;
+    @NotBlank(message = "About is required")
     private String about;
+    @Size(min=10,max = 12,message = "Invalid mobile number")
     private String phoneNumber;
 
 
